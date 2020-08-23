ff2c19a3ff944b89ba93dc4ebd2505b6c44a41c2
Lucas Nogueira <lnogueir@uwaterloo.ca>
2020-08-23 14:51:42 -0400

    started using base64 images to be able to view it on browser

--- modules/hacker_fetcher.py
+++ modules/hacker_fetcher.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
+import base64
+import requests
 import urllib.request
 import ssl
+import os
 
 class HackerFetcher:
   def __init__(self, hacker_id):
@@ -34,7 +37,7 @@ class HackerFetcher:
     contributor_tags = lambda curr_tag: member_tags.find_all('span', class_='user-profile-link') if (member_tags := curr_tag.find(class_='members')) else []
     return [
       {
-        'thumbnail': (img := tag.find('img', class_='software_thumbnail_image')) and img['src'],
+          'thumbnail': (img := tag.find('img', class_='software_thumbnail_image')) and get_base64_img_from(img['src']),
         'href': (a := tag.find('a', class_='link-to-software')) and a['href'],
         'title': (title_div := tag.find('div', class_='software-entry-name')) and title_div.find('h5').text.strip(),
         'description': (description_div := tag.find('div', class_='software-entry-name')) and description_div.find('p').text.strip(),
@@ -49,3 +52,29 @@ class HackerFetcher:
       }
       for tag in project_tags
     ]
+
+
+def get_base64_img_from(url):
+  base64_str = 'data:image/'
+  file_ext = os.path.splitext(url)[1]
+  if file_ext == '.jpg' or file_ext == '.jpeg':
+    base64_str += 'jpeg'
+  elif file_ext == '.png':
+    base64_str += 'png'
+  elif file_ext == '.svg':
+    base64_str += 'svg+xml'
+  elif file_ext == '.gif':
+    base64_str += 'gif'
+  elif file_ext == '.tiff' or file_ext == '.tif':
+    base64_str += 'tiff'
+  elif file_ext == '.ico' or file_ext == '.cur':
+    base64_str += 'x-icon'
+  elif file_ext == '.bmp':
+    base64_str += 'bmp'
+  elif file_ext == '.webp':
+    base64_str += 'webp'
+
+  bin_img_content = base64.b64encode(requests.get(url).content)
+  str_img_content = bin_img_content.decode('utf-8')
+  base64_str += f';base64,{str_img_content}'
+  return base64_str
